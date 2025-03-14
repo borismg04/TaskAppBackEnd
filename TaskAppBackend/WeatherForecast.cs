e72795003ca079da50b15158a43d831cb0e734f1
borismg <borismg04@gmail.com>
2025-03-14 15:28:14 -0500

    Revert "readme"
    This reverts commit 9382d33b0cc5ebd1f4361bbe88b9926d954d7f0c.

--- TaskAppBackend/WeatherForecast.cs
+++ TaskAppBackend/WeatherForecast.cs
@@ -3,6 +3,10 @@ namespace TaskAppBackend
     public class WeatherForecast
     {
 
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
         public string? Summary { get; set; }
     }
 }
